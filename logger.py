506fca39aef8fc85216b1f2bf42e9167518d2431
haydenych <heiheiyu127@gmail.com>
2023-12-06 15:47:40 -0500

    add enable disable mode to logger

--- logger.py
+++ logger.py
@@ -2,22 +2,34 @@ import os
 from datetime import datetime
 
 class Logger:
-    def __init__(self, dirname):
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-
+    def __init__(self, dirname, disable=False):
         self.dirname = dirname
         self.filename = os.path.join(
             self.dirname,
             datetime.now().strftime("%d%m%Y_%H%M%S") + ".txt"
         )
+        self.disable = disable
 
-        open(self.filename, "w").close()
+        if not self.disable:
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
+            open(self.filename, "w").close()
 
     def clear(self):
-        with open(self.filename, "r+") as f:
-            f.truncate(0)
+        if not self.disable:
+            with open(self.filename, "r+") as f:
+                f.truncate(0)
 
     def log(self, msg=""):
-        with open(self.filename, "a") as f:
-            f.write(msg + "\n")
+        if not self.disable:
+            with open(self.filename, "a") as f:
+                f.write(msg + "\n")
+
+    def enable():
+        # TODO
+        pass
+
+    def disable():
+        # TODO
+        pass
