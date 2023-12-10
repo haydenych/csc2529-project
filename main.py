53f4f9d75729099978e65cf99f511d3678b14f77
haydenych <heiheiyu127@gmail.com>
2023-12-09 23:40:54 -0500

    fix main

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import argparse
 from model.LGBPN_BNN import LGBPN_BNN
 from model.SSID_BNN import SSID_BNN
 from model.SSID_LAN import SSID_LAN
+from model.SSID_UNet import SSID_UNet
 
 def main(args):
     model_bnn = SSID_BNN(args.bnn_cfg_path)
-    model = SSID_LAN(args.lan_cfg_path, model_bnn)
-    model.train()
+    model_lan = SSID_LAN(args.lan_cfg_path, model_bnn)
+    model_unet = SSID_UNet(args.unet_cfg_path, model_bnn, model_lan)
 
-    # model_bnn = LGBPN_BNN(args.bnn_cfg_path)
-    # model = SSID_LAN(args.lan_cfg_path, model_bnn)
-    # model.train()
+    model_unet.train()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bnn-cfg-path', type=str, default='./cfgs/train_lan_1/SSID_BNN.json', help="Path to model config")
-    parser.add_argument('--lan-cfg-path', type=str, default='./cfgs/train_lan_1/SSID_LAN.json', help="Path to model config")
+
+    parser.add_argument('--bnn-cfg-path', type=str, default='./cfgs/train_unet/SSID_BNN.json', help="Path to model config")
+    parser.add_argument('--lan-cfg-path', type=str, default='./cfgs/train_unet/SSID_LAN.json', help="Path to model config")
+    parser.add_argument('--unet-cfg-path', type=str, default='./cfgs/train_unet/SSID_UNet.json', help="Path to model config")
 
     args = parser.parse_args()
 
